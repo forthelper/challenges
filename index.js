d21462dd14cb60865efc5e0ddbb146e8a811b223
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2019-09-28 16:59:34 +0200

    challenges tabs

--- index.js
+++ index.js
@@ -1,31 +1,71 @@
 var counter = 1;
+var challengesJSON = {};
 
-$(".cell").each(function() {
+var sidenavList = $("#sidenavcollection");
 
-  this.id = "c" + counter;
 
-    counter++;
-});
+// Transform JSON data into ready form
+function dataReady(data){
+  challengesJSON = data;
 
-$.getJSON( "https://andreymrovol.github.io/fortnitechallenges/challenges.json", function( data ) {
-  console.warn(data);
 
   for(var i=1; i<=10; i++){
     var week = data[i]["Normal"];
 
-    var b = "";
+    let sli = document.createElement('a');
+    sli.innerHTML = data[i]["Title"];
+    sli.className = "collection-item"
+    sli.id = "s" + i;
 
-    var ul = document.createElement('ul');
+    let onc = "loadWeek(" + i + ")";
 
-    for(var o=0; o<week.length; o++){
-      var li = document.createElement('li');
-      li.innerHTML = week[o]["description"]
-      ul.appendChild(li);
-      console.log(week[o]["description"])
-      b = b + week[o]["description"] + '\n';
-    }
+    sli.setAttribute('onclick', onc);
 
-    $("#c" + i).append(ul)
+    sidenavList.append(sli);
 
   }
+  //Display first week on start
+  loadWeek(1);
+}
+
+// Transform array into ul
+function makeListFromArray(array){
+
+var ul = document.createElement('ul');
+  for(var i=0; i<array.length; i++){
+    var li = document.createElement('li');
+
+    let s = array[i]["description"] + " (" + array[i]["objective"] + ")"
+    li.innerHTML = s;
+    ul.appendChild(li);
+  }
+
+  return ul;
+}
+
+// Get all challenges in JSON form
+$.getJSON( "https://andreymrovol.github.io/fortnitechallenges/challenges.json", function( data ) {
+  console.warn(data);
+
+  dataReady(data);
 });
+
+// Display selected week
+function loadWeek(week){
+  let w = challengesJSON[week];
+
+  let normal = w["Normal"];
+  let prestige = w["Prestige"]
+
+  $(".active").removeClass("active");
+  $("#s" + week).addClass("active")
+
+  // $("#Normal").html(w["Normal"]);
+  console.log(w)
+
+  $("#Normal").html("");
+  $("#Prestige").html("");
+
+  $("#Normal").append(makeListFromArray(normal))
+  $("#Prestige").append(makeListFromArray(prestige))
+}
