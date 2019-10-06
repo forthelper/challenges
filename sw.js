e831cf324b9e5549e6a3bf81d759a4ad2d33d494
AndreyMrovol <AndreyMrovol@users.noreply.github.com>
2019-10-06 23:14:20 +0200

    sw

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('site').then(function(cache) {
      return cache.addAll([
-       '/',
-       '/index.html'
+       './',
+       './index.html'
      ]);
    })
  );
